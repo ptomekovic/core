6b05d5d52f40a8511fdf13d3bf0d67ad23309098
lkatzbach <lk@lukaventure.de>
2015-09-24 20:39:17 +0200

    Herokit is bootstrapping now in element html

--- package.js
+++ package.js
@@ -37,7 +37,7 @@ function getFilesFromFolder(packageName, folder) {
 
 Package.describe({
     name: 'herokit:core',
-    version: '0.0.14',
+    version: '0.0.15',
     // Brief, one-line summary of the package.
     summary: 'The Framework for heroes with the best of meteor and angular',
     // URL to the Git repository containing the source code for this package.
