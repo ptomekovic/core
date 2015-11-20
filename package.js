f5ccf3c916d8e62e67be4e1501333a6d46c320aa
lkatzbach <lk@lukaventure.de>
2015-11-20 10:02:07 +0100

    v0.3.1

--- package.js
+++ package.js
@@ -37,7 +37,7 @@ function getFilesFromFolder(packageName, folder) {
 
 Package.describe({
     name: 'herokit:core',
-    version: '0.3.0',
+    version: '0.3.1',
     // Brief, one-line summary of the package.
     summary: 'The Framework for heroes with the best of meteor and angular',
     // URL to the Git repository containing the source code for this package.
